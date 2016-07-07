043afbef89b7cc9d80437ef0fd3d7f69f0883734
manishgdev <manishgdev@gmail.com>
2016-07-07 08:14:53 +0530

    Commiting Java Hello World code for Git

---
+++ JavaHello.java
@@ -0,0 +1,8 @@
+public class JavaHello {
+
+	public static void main(String args[]) {
+
+		System.out.println("Hey There I am getting executed inside the container");
+		System.out.println("Bye");
+	}
+}
